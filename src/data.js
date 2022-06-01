a1d586787aa3ea7284259bf0bacecb2973c42efe
Rittik-12 <rittiksen123321@gmail.com>
2022-06-01 18:18:10 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,79 @@
+export const sliderItems=[
+    {
+        id:1,
+        img:"https://images.unsplash.com/photo-1503376780353-7e6692767b70?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170",
+        title:"Year-end Sale",
+        Description:"Awesome ideas are driven by you.",
+        bg:"f5fafd",
+                
+    },
+    {
+        id:2,
+        img:"https://images.unsplash.com/photo-1614162692292-7ac56d7f7f1e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fHBvcnNjaGV8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+        title:"Season-end Sale",
+        Description:"Awesome ideas are driven by you.",
+        bg:"fcf1ed",
+                
+    },
+    {
+        id:3,
+        img:"https://images.unsplash.com/photo-1494905998402-395d579af36f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8Y2FyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+        title:"big billion days Sale",
+        Description:"Awesome ideas are driven by you.",
+        bg:"fbf0f4",
+                
+    },
+
+];
+
+export const categories=[
+    {
+        id:1,
+        img:"https://media.istockphoto.com/photos/charging-station-for-electric-car-in-concept-of-green-energy-and-eco-picture-id1348631007?b=1&k=20&m=1348631007&s=170667a&w=0&h=dmNgzNuRUl_nZGtbdraAbBC4ZzNZbLO5du7gJxiQBkk=",
+        title:"Electric powered",
+    },
+    {
+        id:2,
+        img:"https://images.unsplash.com/photo-1629241290025-6bb716261f5f?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=60&raw_url=true&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cGV0cm9sJTIwcHVtcHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500",
+        title:"Petrol powered",
+
+    },
+    {
+        id:3,
+        img:"https://media.istockphoto.com/photos/car-with-the-engine-on-hydrogen-fuel-picture-id1315979381?b=1&k=20&m=1315979381&s=170667a&w=0&h=NCyBsdk7LhWcTPiYSF49MCvi7keld1r3byUAtyBifPs=",
+        title:"Hydrogen powered"
+    }
+];
+export const popularProducts = [
+    {
+      id:1,
+      img:"https://images.unsplash.com/photo-1571987502227-9231b837d92a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8dGVzbGF8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+    },
+    {
+      id:2,
+      img:"https://images.unsplash.com/photo-1606016159991-dfe4f2746ad5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8dGVzbGF8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+    },
+    {
+      id:3,
+      img:"https://images.unsplash.com/photo-1541447270888-83e8494f9c06?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fHRlc2xhJTIwbW9kZWwlMjBzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    },
+    {
+      id:4,
+      img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQS1TYK2f2h7mQUrKyVEdOYAizzl5xuK_8W7w&usqp=CAU",
+    },
+    {
+      id:5,
+      img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZv-6m9GCb57Jgsa--UY-MEngDDpk_ZGNF4Q&usqp=CAU",
+    },
+    {
+      id:6,
+      img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLbhI7fAd6KNd-YrfkEiHQiz4panX-nTNEOKzCQEjhkmVAh87MylXimqHXnC7crkyO114&usqp=CAU"  },
+    {
+      id:7,
+      img:"https://media.istockphoto.com/photos/generic-red-suv-on-a-white-background-side-view-picture-id1157655660?b=1&k=20&m=1157655660&s=170667a&w=0&h=DJEYhRel36-0z-dD6xK8PAOLVnIm43gZgYZeuK1M0zo=",
+    },
+    {
+      id:8,
+      img:"https://media.istockphoto.com/photos/generic-white-car-isolated-on-white-background-picture-id1186972461?b=1&k=20&m=1186972461&s=170667a&w=0&h=rDCDVPjC73cZyYLEu87qBRAvDUmJ6tVv5BvLG2Y9ycU=",
+    },
+  ]
